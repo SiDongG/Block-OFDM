3c0eee8180d9adac4270818882b3f6627d356c5c
SiDongG <82831509+SiDongG@users.noreply.github.com>
2022-01-01 16:28:55 -0500

    Update Block_Channel.m

--- Block_Channel.m
+++ Block_Channel.m
@@ -1,5 +1,5 @@
 function [H0,H1]=Block_Channel(L_C,P) %Channel Length of L_C and block size of P
-h=(1/sqrt(2*(L_C)))*(randn(1,L_C)+1i*randn(1,L_C));
+h=(1/sqrt(2*(L_C)))*(randn(1,L_C)+1i*randn(1,L_C));   %This part need some change in that channel order L_C should have channel length of L_C+1 
 %Generated Channel impulse response 
 H0=zeros(P); %Preallocating for speed, H0 is the P by P matrix have the (i,j)th entry h(i-j)
 H1=zeros(P); %Preallocating for speed, H1 is the P by P matrix have the (i,j)th entry h(P+i-j)
