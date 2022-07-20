28c04010f3858016e5d96a170ae6460952bd28a0
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-07-20 18:27:36 -0400

    Add files via upload

--- OFDM3/FadingOFCDM_kernel.m
+++ OFDM3/FadingOFCDM_kernel.m
@@ -10,28 +10,45 @@ loop_Num=100;
 step=2;
 Start=0;
 End=20;
-total=zeros(1,length(0:2:20),3); 
-ratio=zeros(1,length(0:2:20),3);
-for dB=0:4:20
-    disp(dB);
-    SNR=10^(dB/10);
+total=zeros(1,length(Start:step:End),3); 
+ratio=zeros(1,length(Start:step:End),3);
+for SNRdB=Start:step:End
+    disp(SNRdB);
+    SNR=10^(SNRdB/10);
     for U=1:3
         M=4^U;
         for loop=1:loop_Num
             BER=FadingOFCDM(M,SNR);            
-            ratio(1,dB/step-Start/2+1,U)=BER;
-            total(1,dB/step-Start/2+1,U)=total(1,dB/step-Start/2+1,U)+ratio(1,dB/step-Start/2+1,U);
+            ratio(1,SNRdB/step-Start/2+1,U)=BER;
+            total(1,SNRdB/step-Start/2+1,U)=total(1,SNRdB/step-Start/2+1,U)+ratio(1,SNRdB/step-Start/2+1,U);
         end
     end
 end
 total=total/loop_Num;
+%% Theoretical
+BER2=zeros(1,length(Start:step:End),3);
+for SNRdB=Start:step:End
+    for U=1:3
+        M=4^U;
+        EbN0dB=SNRdB-10*log10(log2(M));
+        sqM=sqrt(M);
+        a= 2*(1-power(sqM,-1))/log2(sqM);
+        b= 6*log2(sqM)/(M-1);
+        rn=b*10.^(EbN0dB/10)/2;
+        BER2(1,SNRdB/step-Start/2+1,U) = 0.5*a*(1-sqrt(rn./(rn+1)));
+    end
+end
+
 figure()
 box on; hold on;
 plot(Start:step:End,total(:,:,1),'bx-');
 plot(Start:step:End,total(:,:,2),'rx-');
 plot(Start:step:End,total(:,:,3),'gx-');
+plot(Start:step:End,BER2(:,:,1),'b');
+plot(Start:step:End,BER2(:,:,2),'r');
+plot(Start:step:End,BER2(:,:,3),'g');
 set(gca,'Yscale','log');
-ylim([1e-6 1]);
+ylim([1e-5 1]);
 xlabel('SNR(dB)');
 ylabel('Ber');
-legend('OFDM 4QAM','OFDM 16QAM','OFDM 64QAM')
+legend('OFDM 4QAM','OFDM 16QAM','OFDM 64QAM','1','2','3')
